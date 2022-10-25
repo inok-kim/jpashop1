40367b4b71271d0ac943cb44d0d23482007756d5
iokim <iokim1220@gmail.com>
2022-10-25 21:43:02 +0900

    feat: 주문 서비스 개발 비즈니스 로직이 대부분 엔티티에 있다 (상태와 행위를 모아놓자) => 도메인 모델 패턴
    서비스 계층에서 대부분의 비즈니스 로직을 처리하는 것
    => 트랜잭션 스크립트 패턴

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +12,7 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -25,6 +28,10 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
+    // JPA는 protected, private 생성자까지 허용해줌
+//    protected OrderItem() {
+//    }
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
